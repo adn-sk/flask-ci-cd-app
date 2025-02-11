cf5293cd27060a275fe9c5821cfb0d2ca74e5592
unknown <shaikhad2700@gmail.com>
2025-02-11 17:08:09 +0530

    Updated Flask app for AWS deployments

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to My Flask App</h1><p>My name is Adnan Shaikh</p>"
+    return "Hello, AWS CI/CD Pipeline!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
